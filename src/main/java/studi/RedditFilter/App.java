91deec6a9176952f9615fffd896165d1ad5e1fee
useless1612 <pk1998@gmx.de>
2021-05-27 17:54:05 +0200

    Kombination aller Logik zum zugreifen auf Reddit Posts
    Bisherige Klassen werden kombiniert und die Logik genutzt um Reddit
    Posts zu filtern.

--- src/main/java/studi/RedditFilter/App.java
+++ src/main/java/studi/RedditFilter/App.java
@@ -1,5 +1,10 @@
 package studi.RedditFilter;
 
+import java.io.IOException;
+// import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Hello world!
@@ -7,11 +12,47 @@ package studi.RedditFilter;
  */
 public class App 
 {
-    // Hello here i am
-    public static void main( String[] args )
+    private static final String BASE_URL = "https://oauth.reddit.com/r/";
+
+    // private Posts posts;
+    // private Posts filteredPosts;
+    // private KeepOrRemoveList keepList;
+    // private List<String> removeList;
+    // private String subredditName;
+    // private SortEnum sortEnum;
+    // private JSONObject jo;
+
+    public static void main( String[] args ) throws IOException, JSONException
     {
+        String subredditName = "python";
+        SortEnum sortEnum = SortEnum.NEW;
         System.out.println( "Hello World!" );
-        MainActivity.test();
-        
+        //String test = RedditOAuth.getUserAuthUrl("test").toString();
+        JSONObject jo = RedditOAuth.getToken();
+        System.out.println(jo);
+        JSONObject response = RedditOAuth.getSubreddit(BASE_URL, subredditName, sortEnum, jo.getString("access_token"));
+
+//  Filter Keep Weg
+//  Subreddit ändert sich
+
+        Posts posts = Posts.createFromJsonObject(response);
+        KeepOrRemoveList keepList = new KeepOrRemoveList();
+        keepList.add("python", true);
+        KeepOrRemoveList removeList = new KeepOrRemoveList();
+        removeList.add("fiddle", true);
+        Posts filteredPosts = posts.filter(keepList, removeList);
+
+        System.out.println(response);
+        for (Post p : filteredPosts) {
+            System.out.printf("Filtered POST Name:\n%s\n\n", p.getTitle());
+        }
+
+        for (Post p : posts) {
+            System.out.printf("POST Name:\n%s\n\n", p.getTitle());
+        }
+
+        // SortEnum[] sortEnums = SortEnum.values();
+        // mock data should be passed from user input
+
     }
 }
